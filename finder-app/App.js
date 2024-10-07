b104a9650973daf4f95d65437c92bf030916fab7
MarioB06 <mbozic2006@gmail.com>
2024-10-07 19:25:40 +0200

    Add register page

--- finder-app/App.js
+++ finder-app/App.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import LandingPage from './screens/LandingPage';
+import Register from './screens/Register';
 import Login from './screens/Login';
 import Home from './screens/Home';
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LandingPage">
         <Stack.Screen name="LandingPage" component={LandingPage} />
+        <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
