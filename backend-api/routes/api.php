ca00407115f42891085491cad6973e2dfb06b6e0
MarioB06 <mbozic2006@gmail.com>
2024-10-10 18:58:53 +0200

    setup laravel - new

---
+++ backend-api/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('/items', [ItemController::class, 'index']);
+// Route::post('/items', [ItemController::class, 'store']);
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working correctly!']);
+});
